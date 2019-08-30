dc9aaef364c5a104c32e12a107ccaf5690ad1bf8
Carlo Costantino <costantinoart@gmail.com>
2019-08-30 13:55:51 -1000

    added check and remove features

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 $(function() {
   $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
+
     const newItem = $(this).find('input[name="shopping-list-entry"]').val()
+    
     $('.shopping-list').prepend(
       `<li>
       <span class="shopping-item">${newItem}</span>
@@ -16,5 +18,25 @@ $(function() {
       </div>
       </li>`
       );
+  });
+});
+
+
+$(function() {
+  $('ul').on('click', '.button-label', function(event) {
+    event.stopPropagation();
+
+    const pressed_button = $(this).text()
+    const item = $(this).closest('li').find('.shopping-item')
+
+    // if the button pressed was "check"
+    if (pressed_button === 'check') {
+      // toggle class ".shopping-item__checked" on item
+      $(item).toggleClass('shopping-item__checked');
+      // if the button pressed was "delete"
+    } else if (pressed_button === 'delete') {
+      // remove the list item
+      $(this).closest('li').remove()
+    }
   });
 });
